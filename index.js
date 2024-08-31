8ae5f7b185259eaca094c410ddffc3747490afd0
TonsOfStuff <jtons2008@gmail.com>
2024-08-30 22:02:01 -0500

    attempts

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+const serverRouter = require('./routes/server'); //Telling our app that the router folder and routes folder exists
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views"); //Server rending views
+app.set('layout', 'layouts/layout') //Setting app so that header and footer don't have to be updated each time we make a change
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+app.use("/", serverRouter);
+
+app.listen(process.env.PORT || 3000);
