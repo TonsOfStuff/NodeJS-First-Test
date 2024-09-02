4a630912b0a2e103ebcd0fd94160817d057935dc
TonsOfStuff <jtons2008@gmail.com>
2024-09-02 18:50:08 -0500

    user database integration

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express.Router();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json()); //Middleware for posting
+app.use(express.urlencoded());
+
 const db = require("../models/database")
 
 app.get("/", (req, res) => {
@@ -12,6 +20,20 @@ app.get("/", (req, res) => {
             res.render("users.ejs", {title:"Node.js App Test", action: "list", sampData: data});
         }
     });
+})
+
+app.post("/", (req, res) => {
+    const name = req.body.name;
+    const username = req.body.username;
+
+    let query = `INSERT INTO users (Name, Username) VALUES ('${name}', '${username}')`
+    db.query(query, (error, data) =>{
+        if (error){
+            throw error;
+        }
+    })
+
+    res.redirect(303, "/users")
 })
 
 module.exports = app;
