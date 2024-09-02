3fb03d11153c1a3c61ac3acf7c5e8a797de6d81d
TonsOfStuff <jtons2008@gmail.com>
2024-09-01 20:59:59 -0500

    more stuff

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
 const serverRouter = require('./routes/server'); //Telling our app that the router folder and routes folder exists
+const userRouter = require('./routes/users');
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views"); //Server rending views
@@ -11,5 +12,34 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 
 app.use("/", serverRouter);
+app.use("/users", userRouter);
 
 app.listen(process.env.PORT || 3000);
+
+
+
+
+
+
+
+
+
+
+
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'jtons',
+  password: 'jerryton123',
+  database: 'testdb'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
--- routes/server.js
+++ routes/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("header.ejs");
+    res.render("mainPage.ejs");
 })
 
 module.exports = router;
