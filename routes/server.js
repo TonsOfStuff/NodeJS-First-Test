b22778dc25bab5613794e22c8b6cce48bace31c5
TonsOfStuff <jtons2008@gmail.com>
2024-09-01 23:34:57 -0500

    connection to a database

--- routes/server.js
+++ routes/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get("/", (req, res) => {
+    
+
     res.render("mainPage.ejs");
+    
 })
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express.Router();
+const db = require("../models/database")
 
 app.get("/", (req, res) => {
-    res.render("users.ejs")
+    const query = "SELECT * FROM users";
+
+    db.query(query, (error, data) => {
+        if (error){
+            throw error;
+        }else{
+            res.render("users.ejs", {title:"Node.js App Test", action: "list", sampData: data});
+        }
+    });
 })
 
 module.exports = app;
