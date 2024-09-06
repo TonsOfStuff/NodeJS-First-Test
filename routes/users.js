72bb1985eb714e974233ac6951beb72c59b83d50
TonsOfStuff <jtons2008@gmail.com>
2024-09-06 18:18:14 -0500

    stuff added

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const app = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json()); //Middleware for posting
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 const db = require("../models/database")
 
@@ -22,12 +22,40 @@ app.get("/", (req, res) => {
     });
 })
 
+app.post("/submit", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        console.log("Username or password not provided");
+        return res.status(400).send("Username and password are required");
+    }
+
+    const query = "SELECT * FROM users";
+    db.query(query, (error, data) => {
+        if (error) {
+            console.error("Database query error: ", error);
+            return res.status(500).send("Internal server error");
+        }
+
+        const user = data.find(element => element.Username === username && element.password === password);
+        if (user) {
+            console.log("Log in successful");
+            return res.render("mainPage.ejs"); 
+        } else {
+            console.log("Failed Log In");
+            return res.status(401).send("Login failed: invalid username or password");
+        }
+    });
+});
+
 app.post("/", (req, res) => {
     const name = req.body.name;
     const username = req.body.username;
+    const password = req.body.password;
 
-    let query = `INSERT INTO users (Name, Username) VALUES ('${name}', '${username}')`
-    db.query(query, (error, data) =>{
+    let query = `INSERT INTO users (Name, Username, Password) VALUES ('${name}', '${username}', '${password}')`
+    db.query(query, (error) =>{
         if (error){
             throw error;
         }
